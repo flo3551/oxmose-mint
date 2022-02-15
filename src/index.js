a80511371690eb8c5b8cab62bb61e0527994c470
Florian DA <florian.almeida05@gmail.com>
2022-02-15 15:48:31 +0100

    fix countdown

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function App() {
     const [isSoldOut, setSoldOut] = useState(false);
     const [countdownDate, setCountdownDate] = useState(164503800000);
 
-    const [countdownEnded, setCountdownEnded] = useState(false);
+    const [countdownEnded, setCountdownEnded] = useState(undefined);
     const [countdownDays, setCountdownDays] = useState("00");
     const [countdownHours, setCountdownHours] = useState("00");
     const [countdownMinutes, setCountdownMinutes] = useState("00");
@@ -51,16 +51,11 @@ function App() {
     }
 
     const initCountdown = () => {
-        console.log("INITED");
         let x = setInterval(function () {
-            console.log("SET INTERVAL");
             // Get today's date and time
             var now = new Date().getTime();
 
             // Find the distance between now and the count down date
-            console.log(countdownDate);
-            console.log("now", now);
-
             var distance = countdownDate - now;
 
             // Time calculations for days, hours, minutes and seconds
@@ -74,15 +69,14 @@ function App() {
             setCountdownHours(hours);
             setCountdownMinutes(minutes);
             setCountdownSeconds(seconds);
-            console.log("SET");
             // If the count down is finished, write some text
             if (distance < 0) {
-                console.log("CLEARED");
                 clearInterval(x);
                 setCountdownEnded(true);
                 document.getElementById("mint-right-section").removeAttribute("hidden")
                 document.getElementById("mint-separator-section").removeAttribute("noopacity")
-
+            } else {
+                setCountdownEnded(false);
             }
         }, 1000);
     }
@@ -185,69 +179,73 @@ function App() {
     return (
 
         <div className="flex-1 md:flex flex-col justify-end text-center">
-            {countdownEnded ?
+            {countdownEnded !== undefined &&
                 <div>
-                    {isSoldOut ?
+                    {countdownEnded ?
                         <div>
-                            <p className='text-red font-bold'>SOLD OUT !</p>
+                            {isSoldOut ?
+                                <div>
+                                    <p className='text-red font-bold'>SOLD OUT !</p>
+                                </div>
+                                :
+                                <div>
+                                    {web3 && !loader ?
+                                        <div>
+                                            <div className="bg-gradient-to-r col-12 rounded-full from-indigo-500 to-pink-500 text-white font-bold text-xl sm:text-2xl md:text-3xl flex">
+                                                <input onChange={e => { handleQtyChange(e) }} className="pl-6 w-16 bg-transparent rounded-l-full border-r border-purple-900" placeholder="0" type="number" min="1" max="5" autoFocus />
+                                                <a href="#" onClick={e => { submit(e) }} className="pr-10 pl-5 py-5 block w-full rounded-r-full transition duration-300 ease transform hover:scale-105">
+                                                    Mint 0.3 ETH
+                                                    <div className="text-sm text-center font-normal">+ fees</div>
+                                                </a>
+                                            </div>
+                                            <div className="text-white py-5 font-bold">Minted {nbMintPublic} / 5 NFTs </div>
+                                            <div className="text-white text-xs">Connected with {accounts[0]}</div>
+                                        </div>
+
+                                        :
+                                        <a href="#" onClick={() => { init() }}
+                                            className="px-6 py-3 inline-block text-white rounded-full font-bold bg-gradient-to-r from-indigo-500 to-pink-500 transition duration-300 ease transform hover:scale-105">
+                                            Connect
+                                        </a>
+                                    }
+
+                                    {error && <p className='alert text-red'>{error}</p>}
+                                    {success && <p className='alert text-green'>{success}</p>}
+                                </div>
+                            }
                         </div>
                         :
                         <div>
-                            {web3 && !loader ?
+                            <div className="flex-grow mx-auto px-3 md:container xl:max-w-screen-xl flex flex-col items-center justify-center z-10">
                                 <div>
-                                    <div className="bg-gradient-to-r col-12 rounded-full from-indigo-500 to-pink-500 text-white font-bold text-xl sm:text-2xl md:text-3xl flex">
-                                        <input onChange={e => { handleQtyChange(e) }} className="pl-6 w-16 bg-transparent rounded-l-full border-r border-purple-900" placeholder="0" type="number" min="1" max="5" autoFocus />
-                                        <a href="#" onClick={e => { submit(e) }} className="pr-10 pl-5 py-5 block w-full rounded-r-full transition duration-300 ease transform hover:scale-105">
-                                            Mint 0.3 ETH
-                                            <div className="text-sm text-center font-normal">+ fees</div>
-                                        </a>
-                                    </div>
-                                    <div className="text-white py-5 font-bold">Minted {nbMintPublic} / 5 NFTs </div>
-                                    <div className="text-white text-xs">Connected with {accounts[0]}</div>
-                                </div>
 
-                                :
-                                <a href="#" onClick={() => { init() }}
-                                    className="px-6 py-3 inline-block text-white rounded-full font-bold bg-gradient-to-r from-indigo-500 to-pink-500 transition duration-300 ease transform hover:scale-105">
-                                    Connect
-                                </a>
-                            }
-
-                            {error && <p className='alert text-red'>{error}</p>}
-                            {success && <p className='alert text-green'>{success}</p>}
-                        </div>
-                    }
-                </div>
-                :
-                <div>
-                    <div className="flex-grow mx-auto px-3 md:container xl:max-w-screen-xl flex flex-col items-center justify-center z-10">
-                        <div>
-
-                            <div className="py-4 px-0 md:p-4 bg-opacity-25 rounded-xl">
-                                <h2 className="text-center text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-pink-500">
-                                    Public sale starts in:
-                                </h2>
-                                <div className="mt-10 mx-auto px-4 md:px-0 flex justify-center items-center font-bold text-gray-500 text-center">
-                                    <div className="mx-1 md:mx-6 p-1 py-3 md:p-4 w-1/4 bg-white bg-opacity-5 rounded-2xl">
-                                        <div id="days" className="md:w-20 lg:w-24 md:text-5xl lg:text-7xl text-indigo-400">{countdownDays}</div>
-                                        <div className="uppercase text-xs md:text-base text-pink-400">days</div>
-                                    </div>
-                                    <div className="mx-1 md:mx-6 p-1 py-3 md:p-4 w-1/4 md:text-5xl lg:text-7xl bg-white bg-opacity-5 rounded-2xl">
-                                        <div id="hours" className="md:w-20 lg:w-24 md:text-5xl lg:text-7xl text-indigo-400">{countdownHours}</div>
-                                        <div className="uppercase text-xs md:text-base text-pink-400">hours</div>
-                                    </div>
-                                    <div className="mx-1 md:mx-6 p-1 py-3 md:p-4 w-1/4 md:text-5xl lg:text-7xl bg-white bg-opacity-5 rounded-2xl">
-                                        <div id="minutes" className="md:w-20 lg:w-24 md:text-5xl lg:text-7xl text-indigo-400">{countdownMinutes}</div>
-                                        <div className="uppercase text-xs md:text-base text-pink-400">minutes</div>
-                                    </div>
-                                    <div className="mx-1 md:mx-6 p-1 py-3 md:p-4 w-1/4 md:text-5xl lg:text-7xl bg-white bg-opacity-5 rounded-2xl">
-                                        <div id="seconds" className="md:w-20 lg:w-24 md:text-5xl lg:text-7xl text-indigo-400">{countdownSeconds}</div>
-                                        <div className="uppercase text-xs md:text-base text-pink-400">seconds</div>
+                                    <div className="py-4 px-0 md:p-4 bg-opacity-25 rounded-xl">
+                                        <h2 className="text-center text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-pink-500">
+                                            Public sale starts in:
+                                        </h2>
+                                        <div className="mt-10 mx-auto px-4 md:px-0 flex justify-center items-center font-bold text-gray-500 text-center">
+                                            <div className="mx-1 md:mx-6 p-1 py-3 md:p-4 w-1/4 bg-white bg-opacity-5 rounded-2xl">
+                                                <div id="days" className="md:w-20 lg:w-24 md:text-5xl lg:text-7xl text-indigo-400">{countdownDays}</div>
+                                                <div className="uppercase text-xs md:text-base text-pink-400">days</div>
+                                            </div>
+                                            <div className="mx-1 md:mx-6 p-1 py-3 md:p-4 w-1/4 md:text-5xl lg:text-7xl bg-white bg-opacity-5 rounded-2xl">
+                                                <div id="hours" className="md:w-20 lg:w-24 md:text-5xl lg:text-7xl text-indigo-400">{countdownHours}</div>
+                                                <div className="uppercase text-xs md:text-base text-pink-400">hours</div>
+                                            </div>
+                                            <div className="mx-1 md:mx-6 p-1 py-3 md:p-4 w-1/4 md:text-5xl lg:text-7xl bg-white bg-opacity-5 rounded-2xl">
+                                                <div id="minutes" className="md:w-20 lg:w-24 md:text-5xl lg:text-7xl text-indigo-400">{countdownMinutes}</div>
+                                                <div className="uppercase text-xs md:text-base text-pink-400">minutes</div>
+                                            </div>
+                                            <div className="mx-1 md:mx-6 p-1 py-3 md:p-4 w-1/4 md:text-5xl lg:text-7xl bg-white bg-opacity-5 rounded-2xl">
+                                                <div id="seconds" className="md:w-20 lg:w-24 md:text-5xl lg:text-7xl text-indigo-400">{countdownSeconds}</div>
+                                                <div className="uppercase text-xs md:text-base text-pink-400">seconds</div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    }
                 </div>
             }
 
